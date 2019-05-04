d8ba81cf3a7a64a312cec83dbce68ccd839cd913
plgrover <patrick.l.grover@gmail.com>
2019-05-04 20:54:29 +0000

    First somewhat working version of the code

--- ReadMe.md
+++
@@ -1,3 +0,0 @@
-# pisoMorphFoamGradP
-
-A modification of the pisoFOAM code which includes a pressure gradient and morphing bed. 
--- pisoMorphFoam/pisoMorphFoamGradP.C
+++ pisoMorphFoam/pisoMorphFoamGradP.C
@@ -39,6 +39,10 @@ Description
 #include "turbulentTransportModel.H"
 #include "pisoControl.H"
 #include "fvOptions.H"
+#include "pointMesh.H"
+#include "dynamicFvMesh.H"
+#include "primitivePatchInterpolation.H"
+#include "pointPatchField.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -48,11 +52,13 @@ int main(int argc, char *argv[])
 
     #include "setRootCase.H"
     #include "createTime.H"
-    #include "createMesh.H"
+    #include "createDynamicFvMesh.H"
+    //#include "createMesh.H"
     #include "createControl.H"
     #include "createFields.H"
     #include "createFvOptions.H"
     #include "initContinuityErrs.H"
+    
 
     turbulence->validate();
 
@@ -62,7 +68,56 @@ int main(int argc, char *argv[])
 
     while (runTime.loop())
     {
-        Info<< "Time = " << runTime.timeName() << nl << endl;
+	// - Update of the mesh goes here
+	// Get the patch ID where impingement occurs: patch name must be "wall"
+    	label patchWallID = mesh.boundaryMesh().findPatchID("bottomWall");
+    	const fvPatch& patchWallFaces = mesh.boundary()[patchWallID];
+
+    	Info<< "#--------------- Displacement ---------------# " << endl;
+
+	//Find the reference to the location of pointDisplacement field
+	pointVectorField& PointDisplacement = const_cast<pointVectorField&>
+	(
+		mesh.objectRegistry::lookupObject<pointVectorField>
+		(
+			"pointDisplacement"
+		)
+	);
+	
+	//Get the vector field of the patch
+	vectorField &pDisp=refCast<vectorField>(PointDisplacement.boundaryFieldRef()[patchWallID]);
+
+	//Find the relevant size of the vector and declare a vectorField.
+	int Psize= pDisp.size();
+	vectorField dispVals(Psize);
+
+	Info<< "PSize: " << Psize << nl << endl;
+
+	//- set-up interpolator
+	primitivePatchInterpolation patchInterpolator (mesh.boundaryMesh()[patchWallID] );
+
+	/*scalarField MimpPatch = Mimp.boundaryField()[patchWallID];
+
+	//- perform interpolation
+	scalarField faceValues = patchInterpolator.faceToPointInterpolate(MimpPatch); */
+
+	vectorField &PointPointer = refCast<vectorField>(PointDisplacement.boundaryFieldRef()[patchWallID]);
+	vectorField PointNormalVector = mesh.boundaryMesh()[patchWallID].pointNormals();
+
+ 	forAll(dispVals, index)
+	{
+		dispVals[index].x() = PointPointer[index].x();
+		dispVals[index].y() = PointPointer[index].y(); //- 100. * PointNormalVector[index].y() * runTime.deltaT().value();
+		dispVals[index].z() = PointPointer[index].z() - 0.01 * runTime.deltaT().value();
+	} 
+	
+	PointDisplacement.boundaryFieldRef()[patchWallID] == dispVals;	
+
+	
+	// - End of update meths 
+	mesh.update();
+        
+	Info<< "Time = " << runTime.timeName() << nl << endl;
 
         #include "CourantNo.H"
 
